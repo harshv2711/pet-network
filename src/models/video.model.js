a07066f1119143dd508daa56344e8c49c628ff1e
harsh <harsh@pivotmkg.com>
2025-11-12 23:34:40 +0530

    added jwt

---
+++ src/models/video.model.js
@@ -0,0 +1,47 @@
+import monsgoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        thumbnail: {
+            type: String, // cloudinary url
+            required: true
+        },
+        title: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        description: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        duration: {
+            type: Number, // cloudinary url
+            required: true,
+        },
+        view: {
+            type: Number,
+            default: 0,
+        },
+        isPublished: {
+            type: Boolean,
+            required: true,
+            default: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User" 
+        }
+    }, 
+    {
+        timestamps: true
+    }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const video = monsgoose.model("Video", videoSchema)
